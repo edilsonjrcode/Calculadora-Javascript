c96863c17a5623c4d8aa864bd48a64926bfdeaa0
Edilson Quirino Do Nascimento Júnior <95941206+edilsonjrcode@users.noreply.github.com>
2022-09-11 02:45:18 -0300

    script

---
+++ logica.js
@@ -0,0 +1,25 @@
+function operacao(tipo,valor){
+
+    if (tipo === 'acao'){
+
+        if (valor === 'C'){
+            document.getElementById('resultado').value = ''
+        }
+
+        if (valor === '+' | valor === '-' | valor === '*' | valor === '/' | valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+
+        if (valor === '='){
+            var valor_campo = eval(document.getElementById('resultado').value)
+
+            document.getElementById('resultado').value = valor_campo
+        }
+    }
+
+    else if (tipo === 'valor'){
+        document.getElementById('resultado').value += valor 
+    }
+
+}
+
